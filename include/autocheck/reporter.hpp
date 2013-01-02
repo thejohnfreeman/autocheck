4fa8f4fd7fb1d3deb0c696e2c0268b53417b7ebe
John Freeman <jfreeman@cse.tamu.edu>
2013-01-01 23:50:39 -0600

    gtest reporter

--- include/autocheck/reporter.hpp
+++ include/autocheck/reporter.hpp
@@ -2,6 +2,7 @@
 #define AUTOCHECK_REPORTER_HPP
 
 #include <iostream>
+#include <sstream>
 
 namespace autocheck {
 
@@ -11,6 +12,21 @@ namespace autocheck {
       virtual void failure(size_t tests, const char* reason) const = 0;
   };
 
+  void report_success(std::ostream& out, size_t tests, size_t max_tests) {
+    if (tests < max_tests) {
+      out << "Arguments exhausted after " << tests << " tests."
+        << std::endl;
+    } else {
+      assert(tests == max_tests);
+      out << "OK, passed " << tests << " tests." << std::endl;
+    }
+  }
+
+  void report_failure(std::ostream& out, size_t tests, const char* reason) {
+    out << "Falsifiable, after " << tests << " tests:" << std::endl
+      << reason << std::endl;
+  }
+
   class ostream_reporter : public reporter {
     private:
       std::ostream& out;
@@ -19,20 +35,31 @@ namespace autocheck {
       ostream_reporter(std::ostream& out = std::cout) : out(out) {}
 
       virtual void success(size_t tests, size_t max_tests) const {
-        if (tests < max_tests) {
-          out << "Arguments exhausted after " << tests << " tests."
-            << std::endl;
-        } else {
-          assert(tests == max_tests);
-          out << "OK, passed " << tests << " tests." << std::endl;
-        }
+        report_success(out, tests, max_tests);
       }
 
       virtual void failure(size_t tests, const char* reason) const {
-        out << "Falsifiable, after " << tests << " tests:" << std::endl
-          << reason << std::endl;
+        report_failure(out, tests, reason);
       }
   };
+
+#ifdef ASSERT_TRUE
+
+  class gtest_reporter : public reporter {
+    public:
+      virtual void success(size_t tests, size_t max_tests) const {
+        report_success(std::clog, tests, max_tests);
+        ASSERT_TRUE(true);
+      }
+      virtual void failure(size_t tests, const char* reason) const {
+        std::ostringstream out;
+        report_failure(out, tests, reason);
+        static const bool AUTOCHECK_SUCCESS = false;
+        ASSERT_TRUE(AUTOCHECK_SUCCESS) << out.str();
+      }
+  };
+
+#endif // ASSERT_TRUE
 
 }
 
--- test/check.cpp
+++ test/check.cpp
@@ -16,28 +16,32 @@ struct reverse_prop_t {
 };
 
 TEST(Check, Compiles) {
+  ac::gtest_reporter report;
+
   ac::check<bool>(100, [] (bool b) { return true; },
-      ac::make_arbitrary<bool>());
+      ac::make_arbitrary<bool>(), report);
   ac::check<int>(100, [] (int i) { return i >= 0; },
-      ac::make_arbitrary<int>());
+      ac::make_arbitrary<int>(), report);
   //ac::check<bool>(100, [] (bool b) { return true; }); // ICEs Clang
 
   reverse_prop_t reverse_prop;
 
   ac::check<std::vector<int>>(100, reverse_prop,
-      ac::make_arbitrary(ac::list_of<int>()));
+      ac::make_arbitrary(ac::list_of<int>()), report);
   ac::check<std::vector<int>>(100, reverse_prop,
-      ac::make_arbitrary(ac::cons<std::vector<int>, unsigned int, int>()));
+      ac::make_arbitrary(ac::cons<std::vector<int>, unsigned int, int>()),
+      report);
 
   ac::check<std::vector<char>>(100, reverse_prop,
-      ac::make_arbitrary(ac::list_of<char>()));
+      ac::make_arbitrary(ac::list_of<char>()), report);
 
   std::function<bool (const std::string&)> print_prop =
     [] (const std::string& s) { return !!(std::clog << s << std::endl); };
 
   ac::check<std::string>(100, print_prop,
-      ac::make_arbitrary<std::string>());
+      ac::make_arbitrary<std::string>(), report);
   ac::check<std::string>(100, print_prop,
-      ac::make_arbitrary(ac::make_string_generator<ac::ccPrintable>()));
+      ac::make_arbitrary(ac::make_string_generator<ac::ccPrintable>()),
+      report);
 }
 
