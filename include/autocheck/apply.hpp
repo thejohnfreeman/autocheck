f7d6bd70db3045d36b08ac5b5f51394a8449c226
John Freeman <jfreeman@cse.tamu.edu>
2013-01-25 11:45:19 -0600

    close #1
    Weird issue with const and template parameter packs.

--- include/autocheck/apply.hpp
+++ include/autocheck/apply.hpp
@@ -12,13 +12,16 @@ namespace autocheck {
   /* Const version. */
 
   template <typename F, typename... Args>
-  typename std::result_of<F(const Args&...)>::type
+  /* Cannot put const in the parameter list here for some reason. Breaks in
+   * GCC 4.7.2 and Clang 3.2. Works in Clang 3.3. Doesn't seem right, but
+   * we'll do it this way for now. */
+  typename std::result_of<F(Args&...)>::type
   apply(F f, const std::tuple<Args...>& args) {
     return subapply(f, args, range<sizeof...(Args)>());
   }
 
   template <typename F, typename... Args, int... Is>
-  typename std::result_of<F(const Args&...)>::type
+  typename std::result_of<F(Args&...)>::type
   subapply(F f, const std::tuple<Args...>& args, const range<0, Is...>&) {
     return f(std::get<Is>(args)...);
   }
