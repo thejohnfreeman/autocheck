dd0253cc5bf90836d0968742346f01df5eea09be
Christian Schilling <initcrash@gmail.com>
2014-08-08 15:13:22 +0200

    implement nicer gtest integration for autocheck

--- include/autocheck/reporter.hpp
+++ include/autocheck/reporter.hpp
@@ -92,6 +92,54 @@ namespace autocheck {
       }
   };
 
+  class msg_reporter : public reporter {
+    public:
+      msg_reporter(std::string& msg): msg(msg) {}
+      std::string& msg;
+      virtual void success(size_t tests, size_t max_tests,
+          size_t trivial, distribution&& dist) const
+      {
+        report_success(std::clog, tests, max_tests, trivial, std::move(dist));
+        msg = "";
+      }
+
+      virtual void failure(size_t tests, const char* reason) const {
+        std::ostringstream out;
+        report_failure(out, tests, reason);
+        msg = out.str();
+      }
+  };
+
+struct gtest_checker
+{
+    std::function<void(std::string const&)> expectfn;
+    std::string msg;
+    msg_reporter rep{msg};
+    template<class FN>
+    void operator <<(FN const& fn)
+    {
+        fn(rep);
+        expectfn(msg);
+    }
+
+    template<class FN>
+    gtest_checker& expect(FN const& fn)
+    {
+        expectfn = fn;
+        return *this;
+    }
+};
+
+#define AUTOCHECK_TOKENPASTE2(x, y) x ## y
+#define AUTOCHECK_TOKENPASTE(x, y) AUTOCHECK_TOKENPASTE2(x, y)
+
+#define EXPECT_AUTOCHECK(_rep) autocheck::gtest_checker AUTOCHECK_TOKENPASTE(checker,__LINE__);\
+AUTOCHECK_TOKENPASTE(checker,__LINE__).expect([](std::string const& msg){\
+    bool const AUTOCHECK_SUCCESS = "" == msg;\
+    EXPECT_TRUE(AUTOCHECK_SUCCESS) << msg;\
+})\
+<< [&](ac::msg_reporter const& _rep)
+
 #endif // ASSERT_TRUE
 
 }
